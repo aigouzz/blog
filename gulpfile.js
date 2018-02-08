c64ed31b3f7a5529832911168da91bcd0f9e7d25
guoxunchao <guoxunchao@xiaomi.com>
2018-02-08 15:22:01 +0800

    添加index

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var sass = require('gulp-sass');
+var minify = require('gulp-minify');
+var gulp = require('gulp');
+
+gulp.task('default',function () {
+    gulp.src('scss/*.scss').pipe(sass()).pipe(minify()).pipe(gulp.dest('css/'));
+});
