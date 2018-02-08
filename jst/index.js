fd9c0c236d87b20d9dbbc15d0302e73d0e40dd6e
guoxunchao <guoxunchao@xiaomi.com>
2018-02-08 18:04:30 +0800

    修改index.js

--- jst/index.js
+++ jst/index.js
@@ -1,5 +1,10 @@
+let all = require('block/app');
+
 var app = new Vue({
     el:'#app',
+    component:{
+        all
+    },
     data:{
         title:'我的博客',
         name:'anything you want you can ask for it!',
