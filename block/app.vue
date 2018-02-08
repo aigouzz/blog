022587b9c84a2725b6cb30c9559490064df1e8d6
guoxunchao <guoxunchao@xiaomi.com>
2018-02-08 17:59:14 +0800

    修改app.vue

---
+++ block/app.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrap" id="app">
+        <top></top>
+        <div class="section">
+            <div class="i-slide">
+                <ul>
+                    <li>
+                        <img src="" alt="">
+                    </li>
+                    <li>
+                        <img src="" alt="">
+                    </li>
+                    <li>
+                        <img src="" alt="">
+                    </li>
+                    <li>
+                        <img src="" alt="">
+                    </li>
+                </ul>
+            </div>
+            <div class="i-img">
+                <a href="#" class="i-link">
+                    <img src="" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+    <foot></foot>
+</template>
+<script>
+import top from '@/block/top';
+import foot from '@/block/footer';
+
+export default {
+    name:'app',
+    components:{
+        top,foot
+    }
+}
+
+</script>
+<style>
+
+</style>
