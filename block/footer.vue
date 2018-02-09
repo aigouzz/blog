124a7ad5892f1832322074ceada87e0bdde27bfa
guoxunchao <guoxunchao@xiaomi.com>
2018-02-09 10:22:49 +0800

    修改components

--- block/footer.vue
+++ block/footer.vue
@@ -1,3 +1,10 @@
-<div class="footer">
-    <div class="f-t">版权所有&copy; by guoxc</div>
-</div>
+<template>
+    <div class="footer">
+        <div class="f-t">版权所有&copy; by guoxc</div>
+    </div>
+</template>
+<script>
+    export default {
+        name:'foot'
+    }
+</script>
--- block/top.vue
+++ block/top.vue
@@ -1,3 +1,10 @@
-<div class="header">
-    <a href="/" class="h-i" title="首页">首页</a>
-</div>
+<template>
+    <div class="header">
+        <a href="/" class="h-i" title="首页">首页</a>
+    </div>
+</template>
+<script>
+    export default {
+        name:'top'
+    }
+</script>
